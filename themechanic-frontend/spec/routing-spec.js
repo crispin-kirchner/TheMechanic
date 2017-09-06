9c01e49a992b3cc2c0323e4598a27506aff0bdf8
Crispin Kirchner <kcrispin@web.de>
2017-09-06 23:44:16 +0200

    completed page spec to reflect the current state of the frontend

--- themechanic-frontend/spec/routing-spec.js
+++ themechanic-frontend/spec/routing-spec.js
@@ -10,8 +10,15 @@ const port = 3000;
 
 const browser = 'firefox';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
 
+/**
+ * To be used as the reject function of a promise, to indicate spec
+ * failure if the promise is not resolved.
+ * 
+ * Usage:
+ *     somePromise.then(done, failAndDone(done) );
+ */
 function failAndDone(done) {
   return function(reason) {
     fail(reason);
@@ -19,12 +26,16 @@ function failAndDone(done) {
   }
 }
 
+/**
+ * Navigates the given driver to the url relative to the host and port
+ * configured in the header of the spec.
+ */
 function goTo(driver, url) {
   var urlTail = url === undefined
               ? ''
               : '/#' + url;
   
-  return driver.get('http://' + host + ':' + port + urlTail)
+  return driver.get('http://' + host + ':' + port + urlTail);
 }
 
 describe('Page', function() {
@@ -70,14 +81,67 @@ describe('Page', function() {
   it('should create an alert on navigating the create-order form', function(done) {
     goTo(this.driver, 'create-order').then(() => {
       
-      this.driver.findElements(By.css('div.alert')).then(function(elements) {
+      this.driver.findElements(By.css('div.alert')).then(elements => {
         expect(elements.length).toEqual(1);
         
         elements[0].getText().then(function(text) {
           expect(text.toLowerCase().indexOf('creating')).not.toBe(-1);
           
           done();
-        });
+          
+        }, failAndDone(done) );
+        
+      }, failAndDone(done) );
+    });
+  });
+  
+  it('should create an alert on navigating the dashboard', function(done) {
+    goTo(this.driver, 'mechanic-dashboard').then(() => {
+      this.driver.findElements(By.css('div.alert')).then(elements => {
+        expect(elements.length).toEqual(1);
+        
+        elements[0].getText().then(text => {
+          expect(text.toLowerCase().indexOf('mechanic dashboard')).not.toBe(-1);
+          
+          done();
+          
+        }, failAndDone(done) );
+        
+      }, failAndDone(done) );
+    });
+  });
+  
+  it('should react on sign in', function(done) {
+    var findSignIn = () => {
+      return this.driver.findElement(By.css('#mechanic-mode-toggle a'));
+    }
+    
+    goTo(this.driver).then(() => {
+      var signIn = findSignIn();
+      
+      signIn.getText().then(text => {
+        expect(text).toEqual('Sign in');
+      }, failAndDone(done) );
+      
+      signIn.click().then(() => {
+        signIn = findSignIn();
+        
+        signIn.getText().then(text => {
+          expect(text).toEqual('Sign out');
+          
+          this.driver.findElement(By.id('create-order-button')).click().then(() => {
+            this.driver.findElement(By.tagName('h1')).getText().then(text => {
+              
+              expect(text).toEqual('Create Order');
+              
+              done();
+              
+            }, failAndDone(done) );
+            
+          }, failAndDone(done) );
+          
+        }, failAndDone(done) );
+        
       }, failAndDone(done) );
     });
   });
