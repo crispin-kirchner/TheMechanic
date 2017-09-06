7f9cb9d8825482f78d5df373e8ae9e3c101ee59d
Crispin Kirchner <kcrispin@web.de>
2017-09-06 23:16:18 +0200

    initial test suite

--- themechanic-frontend/spec/routing-spec.js
+++ themechanic-frontend/spec/routing-spec.js
@@ -1,4 +1,5 @@
 const webdriver = require('selenium-webdriver');
+const sd = require('selenium-drivers');
 const By = webdriver.By;
 
 const express = require('express');
@@ -7,50 +8,82 @@ const app = express();
 const host = 'localhost';
 const port = 3000;
 
-describe('Routing', function() {
+const browser = 'firefox';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
+
+function failAndDone(done) {
+  return function(reason) {
+    fail(reason);
+    done();
+  }
+}
+
+function goTo(driver, url) {
+  var urlTail = url === undefined
+              ? ''
+              : '/#' + url;
   
-  beforeAll(function() {
+  return driver.get('http://' + host + ':' + port + urlTail)
+}
+
+describe('Page', function() {
+  
+  beforeAll(function(done) {
     app.use(express.static('public'));
     this.server = app.listen(port, host);
     
-    this.driver = new webdriver.Builder()
-      .forBrowser('firefox')
-      .build();
+    sd.init({ browserName: browser }).then(() => {
+      this.driver = new webdriver.Builder()
+        .forBrowser(browser)
+        .build();
+      
+      done();
+    });
   });
   
-  it('should have the correct title', function() {
-    this.driver.get('http://' + host + ':' + port).then(function() {
+  it('should have the correct title', function(done) {
+    
+    goTo(this.driver).then(() => {
     
       this.driver.getTitle().then( function(title) {
         expect(title).toBe('The Mechanic');
+        
+        done();
       });
       
+    }, failAndDone(done) );
+  });
+  
+  it('should not have a create order button', function(done) {
+    goTo(this.driver).then(() => {
+      this.driver.findElement(By.id('create-order-button')).then(function(element) {
+        
+        fail('create order button exists');
+        
+        done();
+        
+      }, done );
     });
   });
   
-  it('should not have a create order button', function() {
-    this.driver.findElement(By.id('create-order-button')).then(function(element) {
-      fail('create order button exists');
-    });
-  });
-  
-  it('should create an alert on navigating the protected area', function() {
-    this.driver.get('http://' + host + ':' + port + '/#create-order').then(function() {
-      fail('this should really blow up below');
+  it('should create an alert on navigating the create-order form', function(done) {
+    goTo(this.driver, 'create-order').then(() => {
       
       this.driver.findElements(By.css('div.alert')).then(function(elements) {
         expect(elements.length).toEqual(1);
         
-        elements[1].getText().then(function(text) {
-          expect(text.indexOf('dashboard')).not.toBe(-1);
+        elements[0].getText().then(function(text) {
+          expect(text.toLowerCase().indexOf('creating')).not.toBe(-1);
+          
+          done();
         });
-      }, function() {
-        fail('no alert present');
-      });
+      }, failAndDone(done) );
     });
   });
   
   afterAll(function() {
+    this.driver.quit();
     this.server.close();
   });
 });
