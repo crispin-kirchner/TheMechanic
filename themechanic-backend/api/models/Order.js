050ab55ae277143df0f4a20acd79c0255ca6ea64
Crispin Kirchner <kcrispin@web.de>
2017-09-08 23:49:11 +0200

    added auto-generation of order numbers

--- themechanic-backend/api/models/Order.js
+++ themechanic-backend/api/models/Order.js
@@ -9,8 +9,20 @@ module.exports = {
 
   attributes: {
     number: {
-      type: 'integer',
-      autoIncrement: true
+      type: 'string',
+      defaultsTo: () => {
+        // get a random decimal number of length 9
+        var decimal = Math.floor(Math.random() * 1e9).toString();
+        
+        // pad with leading zeros if necessary
+        var pad = '000000000';
+        decimal = pad.substring(0, Math.max(0, pad.length - decimal.length)) + decimal;
+
+        // put into the desired format
+        return decimal.substring(6, 9) + '-' + decimal.substring(0, 6);
+      },
+      unique: true,
+      size: 10
     },
     
     checkInDate: {
